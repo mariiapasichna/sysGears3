096337489a65016c6473d36bd509d35244d3c758
Mariia Pasichna <pasichnayam@gmail.com>
2020-01-14 19:48:11 +0200

    Initial commit

---
+++ src/com/mariiapasichna/Archer.java
@@ -0,0 +1,20 @@
+package com.mariiapasichna;
+
+public class Archer {
+    private static final int STEPS = 20;
+    private int priority;
+    private double startChance;
+
+    public Archer(int priority, double startChance) {
+        this.priority = priority;
+        this.startChance = startChance;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public double probabilityByStep(int step) {
+        return (1 - startChance) / STEPS * step + startChance;
+    }
+}
